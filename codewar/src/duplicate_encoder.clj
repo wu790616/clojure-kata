4f68d3cc52c55c57817485566de9b21cee6b4abe
Wendy <wendy.hj.wu@gmail.com>
2022-10-10 00:46:04 +0800

    Add test from codewar

--- codewar/src/duplicate_encoder.clj
+++ codewar/src/duplicate_encoder.clj
@@ -1,6 +1,7 @@
 (ns duplicate-encoder
   (:require [clojure.string :as str]))
 
+;; https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/clojure
 (defn encode-dups [text]
   (let [lower-text (str/lower-case text)
         count-map (frequencies lower-text)]
